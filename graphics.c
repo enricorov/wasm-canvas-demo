7047aa57c1a40aa32b37215b62faeca25ba28331
enricorov <enricorov@gmail.com>
2021-08-09 09:51:22 +0100

    C funcs

--- graphics.c
+++ graphics.c
@@ -1,8 +1,10 @@
+// #include <graphics.h>
+
 const unsigned int WIDTH = 600;
 const unsigned int HEIGHT = 600;
 unsigned int BUFFER[WIDTH * HEIGHT];
 
-void go() {
+void refresh_buffer(unsigned int color_left, unsigned int color_right) {
     unsigned int screen[600][600];
     int x;
     for (x = 0; x < WIDTH; x++) {
@@ -10,9 +12,9 @@ void go() {
         for (y = 0; y < HEIGHT; y++) {
             unsigned int color;
             if (y < HEIGHT/2) {
-                color = 0xff0000ff; // RGBA little endian
+                color = 0xffff00ff; // RGBA little endian
             } else {
-                color = 0xffff0000; // RGBA little endian
+                color = 0xffffff00; // RGBA little endian
             }
 
             screen[x][y] = color;
